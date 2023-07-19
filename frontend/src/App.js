cb98d9ff7ea386f2bc0a8592e73f9786c097411a
Courtney Cash <84878740+ccash12@users.noreply.github.com>
2023-07-19 12:12:19 -0700

    adding to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,8 @@ export default function App() {
   const [weddingId, setWeddingId] = useState();
   const [guestId, setGuestId] = useState();
   const [guests, setGuests] = useState();
+  const [gifts, setGifts] = useState();
+  const [giftId, setGiftId] = useState();
 
   return (
     <div className="dark:bg-gray-700">
@@ -102,6 +104,21 @@ export default function App() {
               />
             }
           />
+          <Route
+            path="/gifts"
+            element={
+              <DisplayGift 
+                gifts={gifts}
+                setGifts={setGifts}
+                token={token}
+                giftId={giftId}
+                parties={parties}
+                setParties={setParties}
+              />
+            }
+          >
+
+          </Route>
         </Routes>
         {/* <div className="py-10 px-5 md:p-10 h-screen bg-yellow-100 dark:bg-slate-800">
           <Background />
